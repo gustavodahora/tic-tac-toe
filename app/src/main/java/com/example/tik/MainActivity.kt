e49bb5d91f69d2fd6132c36b4b9ad1b2f56a0811
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-06-07 13:43:46 -0300

    Adding a reset button

--- app/src/main/java/com/example/tik/MainActivity.kt
+++ app/src/main/java/com/example/tik/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.tik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.tik.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var checkedThemeGlobal = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun theme(view: View?) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        var checked = binding.darkTheme.isChecked
+        editor.putBoolean("checked", checked).apply()
+        checkedThemeGlobal = checked
+        setTheme()
+    }
+
+    // Set theme
+    fun setTheme() {
+        binding.darkTheme.isChecked = checkedThemeGlobal
+        if (checkedThemeGlobal) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
